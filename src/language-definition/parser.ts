1b04c332bcefeb22a2a27788644c2703fa6446d7
Arne Lecomte <arne.lecomte@gmail.com>
2023-11-24 21:58:38 +0100

    Clean up of the parser

--- src/language-definition/parser.ts
+++ src/language-definition/parser.ts
@@ -1,8 +1,7 @@
 import { EditCodeAction } from "../editor/action-filter";
-import { Actions, InsertActionType, ToolboxCategory } from "../editor/consts";
-import { Argument, FunctionCallStmt, GeneralStatement, KeywordStmt, Statement } from "../syntax-tree/ast";
-import { DataType } from "../syntax-tree/consts";
-import * as codestructs from "./python.json";
+import { InsertActionType, ToolboxCategory } from "../editor/consts";
+import { GeneralStatement, Statement } from "../syntax-tree/ast";
+import * as constructs from "./python.json";
 
 /***
  * TODO: Remove any's and comments between code when API is stable!
@@ -15,17 +14,10 @@ interface JsonCodeStruct {
     name: string;
 }
 
-// const codestructMap = new Map<string, any>();
-
-// for (const codestruct of codestructs) {
-//     console.log(codestruct);
-//     codestructMap.set(codestruct.name, codestruct);
-// }
-
-/* EVERYTHING RELATED TO ACTIONS AND EDITCODEACTIONS */
+/* EVERYTHING RELATED TO ACTIONS AND EDITCODEACTIONS AND AST */
 
 /**
- * Create EditCodeActions in the Action class {@link Actions}
+ * Create EditCodeActions based on the language configuration file.
  */
 export function getAllCodeActions(): EditCodeAction[] {
     // All code actions
@@ -33,75 +25,80 @@ export function getAllCodeActions(): EditCodeAction[] {
     // Variable to create unique ids even when the name is the same
     let toolboxId = 0;
 
+    // First we make sure that all nested codestructs are flattened, i.e.
+    // that all nested implementations are brought to the top level
+    const flattenedConstructs: any[] = []; // TODO: Replace the any type
+
     // Go through all codestructs
-    for (const construct of codestructs) {
-        // If there are implementations, then create an EditCodeAction
-        // for each implementation
+    for (const construct of constructs) {
+        // If there are implementations, bring them to the top level
         if (construct.implementations) {
             // Go through all implementations
             for (const implementation of construct.implementations) {
-                // Create an EditCodeAction
-                const action = new EditCodeAction(
-                    implementation.editorName,
-                    `add-${implementation.name}-btn-${toolboxId++}`,
-                    // NEXT LINE NEEDS TO BE CLEANED UP!
-                    getCodeFunction({...implementation, ...construct}),
-                    InsertActionType.InsertStatement, // EXTRACT; maybe removable?
-                    // InsertActionType.InsertPrintStmt is superfluous
-                    {}, // EXTRACT; context info; maybe extractable from format?
-                    implementation.toolbox,
-                    construct.triggerInsertion, // EXTRACT: character which triggers the insertion in the editor
-                    // Automating? Maybe take last character before a hole or end of statement?
-                    implementation.name, // Match when typing
-                    null // EXTRACT: match regex => Currently only used for VarAssignStmt to
-                    // identify what a valid identifier is => Combine in some form with name
-                );
-                // Add the action to the list
-                editCodeActions.push(action);
+                // Add the implementation to the list of constructs
+                flattenedConstructs.push({ ...implementation, ...construct });
             }
         } else {
-            const action = new EditCodeAction(
-                construct.editorName,
-                `add-${construct.name}-btn-${toolboxId++}`,
-                getCodeFunction(construct),
-                InsertActionType.InsertStatement, // EXTRACT; maybe removable?
-                // InsertActionType.InsertPrintStmt is superfluous
-                {}, // EXTRACT; context info; maybe extractable from format?
-                construct.toolbox,
-                construct.triggerInsertion, // EXTRACT: character which triggers the insertion in the editor
-                // Automating? Maybe take last character before a hole or end of statement?
-                construct.match, // Match when typing
-                null // EXTRACT: match regex => Currently only used for VarAssignStmt to
-                // identify what a valid identifier is => Combine in some form with name
-            );
-            // Add the action to the list
-            editCodeActions.push(action);
+            // If no implementations, add the construct to the list of constructs
+            flattenedConstructs.push(construct);
         }
-        /**
-         * Ideally for cleaner code we would write this as a single statement without
-         * repetition => To look at latet!
-         */
-
-        // If no implementations, create an EditCodeAction for the general codestruct
-
-        // const action = new EditCodeAction()
-        // editCodeActions.push(action)
     }
+
+    // Now we have a list of all given constructs, including the implementations
+
+    // Go through all constructs
+    for (const construct of flattenedConstructs) {
+        const action = new EditCodeAction(
+            construct.editorName,
+            `add-${construct.name}-btn-${toolboxId++}`,
+            getCodeFunction(construct),
+            InsertActionType.InsertStatement, // EXTRACT; maybe removable?
+            // InsertActionType.InsertPrintStmt is superfluous
+            {}, // EXTRACT; context info; maybe extractable from format?
+            construct.toolbox,
+            construct.triggerInsertion, // EXTRACT: character which triggers the insertion in the editor
+            // Automating? Maybe take last character before a hole or end of statement?
+            construct.match, // Match when typing
+            null // EXTRACT: match regex => Currently only used for VarAssignStmt to
+            // identify what a valid identifier is => Combine in some form with name
+        );
+        // Add the action to the list
+        editCodeActions.push(action);
+    }
+    /**
+     * Ideally for cleaner code we would write this as a single statement without
+     * repetition => To look at latet!
+     */
+
+    // If no implementations, create an EditCodeAction for the general codestruct
+
+    // const action = new EditCodeAction()
+    // editCodeActions.push(action)
     return editCodeActions;
 }
 
+/**
+ * Expands the toolboxCategories with the given EditCodeActions. If one of the EditCodeActions
+ * contains a new category, then a new ToolboxCategory is created and added to the list.
+ *
+ * @param toolboxCategories - List of existing toolbox categories. This list will be updated
+ * in place
+ * @param editCodeActions - List of the newly created EditCodeActions, so the ones that
+ * are loaded dynamically, and thus do not yet have a category
+ */
 export function addEditCodeActionsToCategories(
     toolboxCategories: ToolboxCategory[],
     editCodeActions: EditCodeAction[]
 ): void {
     for (const action of editCodeActions) {
+        console.log(action);
         const currentCategory: string = action.documentation.category;
         if (toolboxCategories.some((category) => currentCategory === category.displayName)) {
             toolboxCategories.find((category) => currentCategory === category.displayName).addEditCodeAction(action);
         } else {
             const newCategory = new ToolboxCategory(
                 currentCategory,
-                currentCategory.toLocaleLowerCase() + "-ops-toolbox-group",
+                currentCategory.toLowerCase() + "-ops-toolbox-group",
                 [action]
             );
             toolboxCategories.push(newCategory);
@@ -112,26 +109,11 @@ export function addEditCodeActionsToCategories(
 /**
  * Builds the function to return a FunctionCallStmt from a list of arguments.
  *
- * @param name - name of the codestruct e.g. "print"
- * @param str_args - arguments of the function call
- * @returns - a function that returns a FunctionCallStmt
+ * @param construct - An object containing the information to build the GeneralStatement
+ * @returns - a function that returns a GeneralStatement
  */
 function getCodeFunction(construct): () => Statement {
-    console.log(construct);
     return () => new GeneralStatement(construct);
-    // if (name === "break")
-    //     return () =>
-    //         new KeywordStmt(
-    //             name,
-    //             null,
-    //             null,
-    //             (c) => true
-    //         ); /* last argument is mandatory even though it is marked as optional */
-    // const args: Argument[] = [];
-    // for (const arg of str_args) {
-    //     args.push(new Argument([DataType.Any /* should be changed! */], arg.name, arg.optional));
-    // }
-    // return () => new FunctionCallStmt(name, args);
 }
 
 /* EVERYTHING RELATED TO ... */
