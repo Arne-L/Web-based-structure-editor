b7013e01d182560abdc6a876a186a779c2d595a4
Arne Lecomte <arne.lecomte@gmail.com>
2024-07-17 00:24:38 +0200

    Added functionality to export button

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,11 @@ import { initializeEditor as initialiseEditor } from "./editor/language-toggle";
 import { Loader } from "./language-definition/parser";
 import { Actions } from "./editor/consts";
 
-console.log("index")
+console.log("index");
 
 // @ts-ignore
-self.MonacoEnvironment = { // self == window, so we define a MonacoEnvironment property on the window object
+self.MonacoEnvironment = {
+    // self == window, so we define a MonacoEnvironment property on the window object
     getWorkerUrl: function (moduleId, label) {
         if (label === "json") {
             return "./json.worker.bundle.js";
@@ -30,29 +31,51 @@ self.MonacoEnvironment = { // self == window, so we define a MonacoEnvironment p
     },
 };
 
-
 await initialiseEditor();
 
 // retrieveUser();
-let currLanguage;
 let nova = new Module("editor");
 const runBtnToOutputWindow = new Map<string, string>();
 runBtnToOutputWindow.set("runCodeBtn", "outputDiv");
 
-const languageToggle = <HTMLSelectElement>document.getElementById("toggleLanguageBtn")
+const languageToggle = <HTMLSelectElement>document.getElementById("toggleLanguageBtn");
 languageToggle.addEventListener("change", handleToggle);
 
-async function handleToggle() { 
-    console.log("Language changed to: ", this.value);
-    currLanguage = this.value;
+// Handle export button
+document.getElementById("exportCodeBtn").addEventListener("click", () => {
+    const code = nova.editor.monaco.getValue();
+    download(code, "code.py", "text/plain");
+    console.log("Exported code", code);
+});
+
+async function handleToggle() {
     await initialiseEditor(this.value);
     Actions.reset();
-    console.log("Editor initialised", Loader.instance)
     nova = new Module("editor");
 
     // Reattach event listener after previous one has been deleted
     const languageToggle = <HTMLSelectElement>document.getElementById("toggleLanguageBtn");
     languageToggle.addEventListener("change", handleToggle);
- }
 
-export { nova, runBtnToOutputWindow, currLanguage };
+    // Handle export button
+    document.getElementById("exportCodeBtn").addEventListener("click", () => {
+        const code = nova.editor.monaco.getValue();
+        download(code, "code.py", "text/plain");
+    });
+}
+
+function download(data, filename, type) {
+    const file = new Blob([data], { type: type });
+    const a = document.createElement("a"),
+        url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(() => {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }, 0);
+}
+
+export { nova, runBtnToOutputWindow };
